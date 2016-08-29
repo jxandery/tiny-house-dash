88f59ccd64c188db37bebe256c6e51299d8268fb
Jack Yeh <jack.yeh@YehJ.local>
2016-08-29 12:52:24 -0600

    improve variable naming

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :clients_coaches, class_name: 'CoachClient',
+  has_many :client_coaches, class_name: 'CoachClient',
                            foreign_key: 'client_id',
                            dependent: :destroy
   has_many :coach_clients, class_name: 'CoachClient',
                            foreign_key: 'coach_id',
                            dependent: :destroy
-  has_many :coaches, through: :clients_coaches, source: :coach
+  has_many :coaches, through: :client_coaches, source: :coach
   has_many :clients, through: :coach_clients, source: :client
 
   def coach
